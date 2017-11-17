fc7ae4b0be5fc8bfe6836e62fd59ef827066a3e8
mystreamforyou <309410364@qq.com>
2017-11-17 18:42:54 +0800

    init

--- app/src/main/java/com/jack/tguide/main/home/HomeFragment.kt
+++ app/src/main/java/com/jack/tguide/main/home/HomeFragment.kt
@@ -1,9 +1,17 @@
 package com.jack.tguide.main.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import com.bigkoo.convenientbanner.ConvenientBanner
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator
 import com.jack.tguide.R
 import com.jack.tguide.base.BaseMvpFragment
+import kotlinx.android.synthetic.main.fragment_home.*
+import android.widget.ImageView.ScaleType
+import com.bigkoo.convenientbanner.holder.Holder
+
 
 /**
  * Description: TGuide
@@ -16,6 +24,8 @@ class HomeFragment : BaseMvpFragment<HomeView, HomePresenter>(), HomeView {
     override val mResId: Int get() = R.layout.fragment_home
     override val mPresenter: HomePresenter get() = HomePresenter()
 
+    private var localImages: ArrayList<Int> = ArrayList()
+
     companion object {
         fun newInstance(): HomeFragment {
             val args = Bundle()
@@ -27,10 +37,34 @@ class HomeFragment : BaseMvpFragment<HomeView, HomePresenter>(), HomeView {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadTestDatas()
         initView()
     }
 
     private fun initView() {
+        var a= CBViewHolderCreator {
+            @Override
+            fun createHolder(): LocalImageHolderView {
+                return LocalImageHolderView()
+            }
+        }
+
+        convenientBanner.setPages(a, localImages)
+                .setPageIndicator(intArrayOf(R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused))
+                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+//        convenientBanner.setManualPageable(false);//设置不能手动影响
+
+    }
+
+    private fun loadTestDatas() {
+        //本地图片集合
+        localImages.add(R.drawable.ic_test_0)
+        localImages.add(R.drawable.ic_test_1)
+        localImages.add(R.drawable.ic_test_2)
+        localImages.add(R.drawable.ic_test_3)
+        localImages.add(R.drawable.ic_test_4)
+        localImages.add(R.drawable.ic_test_5)
+        localImages.add(R.drawable.ic_test_6)
     }
 
 }
